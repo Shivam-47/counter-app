261b5e406c262dce8bc3a261cbd84cb610029f0c
Shivam Handa <49410579+Shivam-47@users.noreply.github.com>
2020-11-02 13:36:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,13 +5,4 @@ app.get('/',function(req,res){
     res.sendFile(__dirname+'/test.html');
 })
 
-// app.get('/title_image.jpg',function(req,res){
-//     res.sendFile('title_image.jpg');
-// })
-
-// app.get('/hare_rama.mp3',function(req,res){
-//     res.sendFile('hare_rama.mp3');
-// })
-
-
 app.listen(process.env.PORT || 3000) ;
