4c9aa0d29ec6c624a3d49535496ede51e0684eb9
Shivam Handa <sh17@iitbbs.ac.in>
2020-11-02 13:06:01 +0530

    #3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express= require('express');
 var app=express();
 
 app.get('/',function(req,res){
-    res.sendFile(__dirname+'/views/test.html');
+    res.sendFile(__dirname+'test.html');
 })
 
 app.listen(3000 || process.env.PORT);
